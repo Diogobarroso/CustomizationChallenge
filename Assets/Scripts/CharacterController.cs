745146ecc80f44775a26e65bfa947c771d2a0023
Diogo Barroso <ei11105@fe.up.pt>
2018-01-03 15:26:54 +0000

    Refactored code. Animation changes working (only right arrow though). Still no persistence.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	private string name;
+	private bool edit = false;
+	private Animation anim;
+	private float interval;
+	public float maxInterval = 4f;
+	public int activated = 0;
+	public List<GameObject> anims; //storing the different animations we have
+	public string[] animations = {"Jumping","Looking Around","Yelling"}; //the animations included in the project
+	void Start () {
+		name = transform.GetComponentInChildren<TextMesh>().text;
+		anim = GetComponentInChildren<Animation>();
+
+		LoadAnimations();
+	}
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		interval += Time.deltaTime;
+
+		if(interval >= maxInterval)
+		{
+			anim.Play();
+			interval = 0;
+		}
+	}
+
+	void changeName(string newName)
+	{
+		name = newName;
+	}
+
+	public void OnMouseDown()
+	{
+		if(SceneManager.GetActiveScene().buildIndex == 0) //if this is the display scene
+		{
+			edit = true;
+			SceneManager.LoadScene(1);
+		}
+	}
+
+	public bool getEdit()
+	{
+		return edit;
+	}
+
+	public void setEdit(bool state)
+	{
+		edit = state;
+	}
+
+	void LoadAnimations()
+	{
+		for(int i = 0 ; i < animations.Length; i++)
+		{
+			anims.Add(Instantiate(Resources.Load(name + "@" + animations[i])) as GameObject);
+			Debug.Log("loaded " + name + " " + animations[i]);
+			anims[i].transform.SetParent(transform);
+			anims[i].transform.localPosition = new Vector3(0,0,0);
+			anims[i].SetActive(false);
+		}
+
+		anims[0].SetActive(true);
+		anim = anims[0].GetComponentInChildren<Animation>();
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject currentModel; //The model we're editing
+	public GameObject Canvas;
+	public Button rightButtonPrefab;
+	public Button leftButtonPrefab;
+	CharacterController controller; //the controller of the character we're editing
+	void Start ()
+	{
+		GameObject[] characters = GameObject.FindGameObjectsWithTag("Player");
+		foreach(GameObject character in characters)
+		{
+			if(character.GetComponent<CharacterController>().getEdit()) //check if this is the model we want to edit
+			{
+				currentModel = character;
+				currentModel.transform.position = new Vector3(0,0,0); //place the model in the origin
+			}
+			else
+				character.SetActive(false);
+		}
+		controller = currentModel.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void BackButton()
+	{
+		currentModel.GetComponent<CharacterController>().setEdit(false);
+		SceneManager.LoadScene(0);
+	}
+
+	public void EditName()
+	{
+		/*string newName = "";
+		InputField newNameField = 
+		newNameField.text = "New name";
+		newNameField.transform.position = new Vector3(0,0,0);
+		*/
+	}
+
+	public void EditAnim()
+	{
+		//TODO: edit the animation of the model
+		Button leftButton = (Button)Instantiate(leftButtonPrefab);
+		Button rightButton = (Button)Instantiate(rightButtonPrefab);
+		leftButton.transform.SetParent(Canvas.transform,false);
+		rightButton.transform.SetParent(Canvas.transform,false);
+
+		leftButton.onClick.AddListener(LeftArrow);
+		rightButton.onClick.AddListener(RightArrow);
+		Debug.Log("AFTER BUTTONS");
+	}
+
+	public void LeftArrow()
+	{
+		controller.anims[controller.activated].SetActive(false);
+		if(controller.activated == 0)
+			controller.activated = controller.anims.Count;
+		else
+			controller.activated -= 1;
+		
+		controller.anims[controller.activated].SetActive(true);
+	}
+
+	public void RightArrow()
+	{
+		controller.anims[controller.activated].SetActive(false);
+		if(controller.activated == controller.anims.Count - 1)
+			controller.activated = 0;
+		else
+			controller.activated += 1;
+		
+		controller.anims[controller.activated].SetActive(true);
+	}
+}
